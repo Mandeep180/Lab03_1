d9e147cdc702ffdad2e728c4b4e5c5e8cad50cda
Mandeep Kaur <mkaur864@my.centennialcollege.ca>
2022-09-26 02:45:33 -0400

    new changes

--- routes/index.js
+++ routes/index.js
@@ -3,58 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  res.render('index', { title: 'Home' });
 });
+
 
 /* GET home page. */
-router.get('/homepage', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
+
+
 
 /* GET About Me page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
-});
-
-/* GET About Me page. */
-router.get('/myself', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 
-
-/* GET Projects page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
-});
 
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 
-
-/* GET services page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
-
-
-/* GET Contact us */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
-});
 
 /* GET Contact us */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
